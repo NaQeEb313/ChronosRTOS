5dec2ffd33ca8ad85e6de53f08126e10f5a2d942
Moksh <mandaliamoksh@gmail.com>
2026-09-15 21:19:31 +0530

    Implemented Round Robin Priority Scheduler

--- Core/Inc/scheduler.h
+++ Core/Inc/scheduler.h
@@ -9,6 +9,14 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include "task.h"
 
+void Scheduler_Init(void) ;
+
+TCB *Scheduler_Select_Next(void) ;
+
+// void Scheduler_Start(void) ;
+
+// void Scheduler_Tick(void) ;
 
 #endif /* SCHEDULER_H */
--- Core/Src/scheduler.c
+++ Core/Src/scheduler.c
@@ -6,3 +6,48 @@
  * Description : Round-robin scheduler implementation.
  ******************************************************************************/
 
+
+#include "task.h"
+#include "scheduler.h"
+#include <stddef.h>
+
+TCB *current_task = NULL ;
+TCB *next_task = NULL ;
+TCB *idle_task = NULL ;
+
+
+void Scheduler_Init(void) {
+    current_task = Task_Get_Current() ;
+    idle_task = Task_Get_Idle() ;
+} ;
+
+TCB *Scheduler_Select_Next(void) {
+    TCB* start = NULL ;
+    TCB* node = NULL ;
+    TCB *highest = NULL ;
+
+    start = current_task ;
+    node = start->next ;
+    highest = start->next ;
+
+    do {
+        if((highest->task_priority) < (node->task_priority)) {
+            highest = node ;
+            next_task = highest ;
+            node = node->next ;
+            continue ;
+        }
+
+        if (node == idle_task && start == idle_task) {
+            highest = idle_task ;
+            next_task = highest ;
+            return next_task ;
+        }
+        node = node->next ;
+    } while(node != start) ;
+    
+    if(next_task == NULL) {
+        next_task = highest ;
+    }
+    return next_task ;
+} ;
